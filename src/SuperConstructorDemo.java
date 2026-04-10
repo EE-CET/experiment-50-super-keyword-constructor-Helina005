185736b2c603105d815e20afdf105704e6592c39
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 19:19:02 +0530

    SuperConstructorDemo

--- src/SuperConstructorDemo.java
+++ src/SuperConstructorDemo.java
@@ -1,17 +1,25 @@
+
+
+
 class Person {
-    // TODO: Create a constructor Person()
-    // Print "Person class"
+    // Constructor of the parent class
+    public Person() {
+        System.out.println("Person class");
+    }
 }
 
 class Student extends Person {
-    // TODO: Create a constructor Student()
-    // Call super() first (this is implicit, but explicit is good practice)
-    // Print "Student class"
+    // Constructor of the child class
+    public Student() {
+        // The super() call must be the first statement in the constructor
+        super(); 
+        System.out.println("Student class");
+    }
 }
-
 public class SuperConstructorDemo {
     public static void main(String[] args) {
-        // TODO: Create an object of Student class
-        // Creating the object will automatically invoke constructors
+        // Creating an object of the child class automatically triggers 
+        // the constructor chain (Student -> Person)
+        Student student = new Student();
     }
 }
